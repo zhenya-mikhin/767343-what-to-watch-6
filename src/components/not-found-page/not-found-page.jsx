71ad2c52f815ac8839d8b2ec40c92f93b6f3b42e
zhenya-mikhin <zhenya.mikhin@bk.ru>
2021-02-15 17:35:49 +0300

    add all components

---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+      </header>
+
+      <main>
+        <h1>404. Page not found</h1>
+        <Link to="/">Вернуться на главную</Link>
+      </main>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default NotFoundPage;
