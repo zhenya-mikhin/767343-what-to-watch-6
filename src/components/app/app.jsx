fe80d24954226952283e5a571fa551b2b6cd3f7a
zhenya-mikhin <zhenya.mikhin@bk.ru>
2021-02-15 18:01:44 +0300

    add routing in app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
 import MainPage from '../main/main.jsx';
+import AddReview from '../add-review/add-review.jsx';
+import Film from '../film/film.jsx';
+import MyList from '../mylist/mylist.jsx';
+import NotFoundPage from '../not-found-page/not-found-page.jsx';
+import Player from '../player/player.jsx';
+import SignIn from '../sign-in/sign-in.jsx';
+
 import PropTypes from 'prop-types';
 
 const App = (props) => {
   const {title, genre, release} = props;
 
   return (
-    <MainPage
-      title={title}
-      genre={genre}
-      release={release}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage title={title} genre={genre} release={release} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -11,7 +11,7 @@ const Player = () => {
         <div className="player__controls-row">
           <div className="player__time">
             <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style="left: 30%;">Toggler</div>
+            <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
           </div>
           <div className="player__time-value">1:30:29</div>
         </div>
