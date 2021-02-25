12af1e9cd89354c262d0758c907a2fb4f3a130a9
zhenya-mikhin <zhenya.mikhin@bk.ru>
2021-02-25 15:43:42 +0300

    add mocks

---
+++ src/mocks/reviews.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 1,
+    user: {
+      id: 1,
+      name: `Kate Muir`
+    },
+    rating: 8.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2019-05-08T14:13:56.569Z`
+  },
+  {
+    id: 2,
+    user: {
+      id: 2,
+      name: `Brian Marshnz`
+    },
+    rating: 8.9,
+    comment: `I actually found some of the scenes to be quite frightening, especially when the vampires are chasing one of their victims through their home! This scene was one of my favourites.`,
+    date: `2014-06-08T16:10:21.569Z`
+  },
+  {
+    id: 3,
+    user: {
+      id: 3,
+      name: `Seb Moz`
+    },
+    rating: 8.9,
+    comment: `This is Tarantino's masterpiece, there's no other way to say it. It has arguably one of the smartest scripts I've ever seen. The story, which is non-linear, is so well constructed it takes several viewings to grasp it all.`,
+    date: `2020-11-08T11:11:43.569Z`
+  },
+  {
+    id: 4,
+    user: {
+      id: 4,
+      name: `Dennis Schwartz`
+    },
+    rating: 7.7,
+    comment: `Everyone is after some easy money. Like the director’s other film, this has no strong role for a woman.`,
+    date: `2009-07-08T18:45:56.569Z`
+  },
+  {
+    id: 5,
+    user: {
+      id: 5,
+      name: `Erik Childress`
+    },
+    rating: 5.7,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2019-05-08T09:17:56.569Z`
+  },
+  {
+    id: 6,
+    user: {
+      id: 6,
+      name: `Austin Chronicle`
+    },
+    rating: 8.3,
+    comment: `ith his fifth feature film, Anderson boards another train of ironic whimsy, although this time the train is quite literal and not just the train of thought in his head.`,
+    date: `2019-05-08T20:20:56.569Z`
+  },
+  {
+    id: 7,
+    user: {
+      id: 7,
+      name: `Eric Snider`
+    },
+    rating: 7.9,
+    comment: `I’ve often been tempted when reviewing sequels that do nothing more than mimic their predecessors to simply reprint what I wrote on the original film. “The Darjeeling Limited” marks the first time I’ve considered doing that for a non-sequel.`,
+    date: `2016-01-08T10:28:56.569Z`
+  },
+];
